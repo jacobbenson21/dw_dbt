432b34a1987d3dfa3c2b09533af765a8ac614bd0
Jacob Benson <bensonjacob728@gmail.com>
2025-03-29 04:54:27 +0000

    new commit

--- models/samssubs/fact_webtraffic.sql
+++ models/samssubs/fact_webtraffic.sql
@@ -9,12 +9,15 @@ SELECT
     e.event_key,
     p.page_key,
     ts.traffic_source_key,
-    COUNT(wte.*) AS interactioncount
+    COUNT(wte.*) AS interactioncount,
+    u.user_key
 
 FROM {{ source('websource_landing', 'web_traffic_events') }} wte
 INNER JOIN {{ ref('trafficsource_dim') }} ts ON wte.traffic_source=ts.traffic_source
 INNER JOIN {{ ref('page_dim') }} p ON wte.page_url = p.page_url
 INNER JOIN {{ ref('event_dim') }} e ON wte.event_name = e.event_name
 INNER JOIN {{ ref('date_dim') }} d ON CAST(wte.event_timestamp AS DATE) = d.date_day
+INNER JOIN {{ ref('user_dim') }} u ON wte.user_email=u.user_email
 
 GROUP BY all
+
